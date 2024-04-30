89a20aba7a8e6d7e0502291fa7b45facddf22616
erick.fraga <erick.fraga@globalsys.com.br>
2024-04-30 09:35:25 -0300

    feat: jwt auth token

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -12,14 +12,20 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
   const { email, password } = authenticateBodySchema.parse(request.body)
 
   try {
-
-
     const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
     })
+
+    const token = await reply.jwtSign({}, {
+      sign: {
+        sub: user.id
+      }
+    })
+    return reply.status(200).send({ token })
+
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message })
@@ -27,5 +33,4 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
     throw err
   }
 
-  return reply.status(200).send({})
 }
